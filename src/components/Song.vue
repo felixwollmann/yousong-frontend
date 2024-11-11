8fb4f29d597c4b71180a7963cd860c2463ed3ba1
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-11-11 10:23:32 +0100

    add exercise 2: simple rest service

---
+++ src/components/Song.vue
@@ -0,0 +1,14 @@
+<script setup>
+const props = defineProps({
+  song: Object
+})
+</script>
+
+<template>
+  <div class="bg-slate-200 p-4 rounded">
+    <h2 class="font-bold">{{ song.title }}</h2>
+    <p>{{ song.artist }}</p>
+    <p>{{ song.genre }}</p>
+    <p>{{ song.length }} Sekunden</p>
+  </div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+const songs = await (await fetch('http://localhost:8080/api/songs')).json()
+import Song from '@/components/Song.vue'
 </script>
 
 <template>
+  {{ songs }}
   <main>
-    <TheWelcome />
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2 p-2">
+      <div v-for="song in songs">
+        <Song :song></Song>
+      </div>
+    </div>
   </main>
 </template>
