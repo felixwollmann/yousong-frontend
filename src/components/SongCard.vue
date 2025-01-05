bfb3b3d67dd2e313065a767d20e9fcf50e1b460e
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2025-01-05 19:02:39 +0100

    add music files

--- src/components/SongCard.vue
+++ src/components/SongCard.vue
@@ -1,13 +1,41 @@
 <script setup>
+import { useDropZone } from '@vueuse/core'
+import { useTemplateRef } from 'vue'
+
 const props = defineProps({
   song: Object
 })
 
-const emit = defineEmits(['delete', 'edit'])
+const emit = defineEmits(['delete', 'edit', 'play'])
+
+const dropZoneElement = useTemplateRef('dropZoneElement')
+
+const { isOverDropZone } = useDropZone(dropZoneElement, {
+  /** @param {File[] | null} files */
+  async onDrop(files) {
+    const formData = new FormData()
+    formData.append('file', files[0])
+
+    await fetch(`http://localhost:8080/api/songs/${props.song.id}/audio`, {
+      method: 'POST',
+      body: formData
+    })
+  },
+  // specify the types of data to be received.
+  dataTypes: ['audio/mpeg', 'audio/mp3'],
+  // control multi-file drop
+  multiple: false,
+  // whether to prevent default behavior for unhandled events
+  preventDefaultForUnhandled: false
+})
 </script>
 
 <template>
-  <div class="bg-slate-100 p-4 rounded relative">
+  <div
+    class="bg-slate-100 p-4 rounded relative transition-colors"
+    :class="{ '!bg-blue-300': isOverDropZone }"
+    ref="dropZoneElement"
+  >
     <h2 class="font-bold">{{ song.title }}</h2>
     <p>{{ song.artist }}</p>
     <p>{{ song.genre }}</p>
@@ -20,6 +48,9 @@ const emit = defineEmits(['delete', 'edit'])
     </button>
     <button @click="emit('edit')" class="absolute top-0 right-0 bg-slate-300 px-1 py-0.5 rounded">
       Edit
+    </button>
+    <button @click="emit('play')" class="absolute bottom-0 left-0 bg-slate-300 px-1 py-0.5 rounded">
+      Play
     </button>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, useTemplateRef, computed, watch } from 'vue'
+import { ref, useTemplateRef, computed, watch, nextTick } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import SongCard from '@/components/SongCard.vue'
 
@@ -98,14 +98,48 @@ const songName = ref('')
 const artist = ref('')
 const genre = ref('')
 const length = ref(0)
+
+import { useSound } from '@vueuse/sound'
+
+const playingSong = ref()
+const playingSongSourceUrl = computed(() =>
+  playingSong.value ? `http://localhost:8080/api/songs/${playingSong.value.id}/audio` : undefined
+)
+
+const { pause, isPlaying, play, stop } = useSound(playingSongSourceUrl, {
+  format: 'mp3',
+  interrupt: true
+})
+
+async function playSong(song) {
+  try {
+    stop()
+  } catch (e) {
+    /* empty */
+    console.error(e)
+  }
+  playingSong.value = song
+  await nextTick()
+  try {
+    play()
+  } catch (e) {
+    /* empty */
+    console.error(e)
+  }
+}
 </script>
 
 <template>
   <main class="p-2">
     <h1 class="text-xl">YouSong</h1>
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">
-      <div v-for="song in songs">
-        <SongCard :song @delete="deleteSong(song)" @edit="openDialogForEditing(song)"></SongCard>
+      <div v-for="song in songs" :key="song.id">
+        <SongCard
+          :song
+          @delete="deleteSong(song)"
+          @edit="openDialogForEditing(song)"
+          @play="playSong(song)"
+        ></SongCard>
       </div>
     </div>
     <div class="" v-if="songs.length === 0">
@@ -115,6 +149,51 @@ const length = ref(0)
     <button class="fixed right-10 bottom-10 bg-blue-200" @click="openDialogForNewSong">
       Add a song
     </button>
+    <!-- Music player -->
+    <div class="fixed bottom-0 left-0 p-10 rounded-tr-lg bg-fuchsia-700">
+      <svg
+        v-if="!isPlaying"
+        @click="play()"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          fill-opacity="0"
+          stroke="currentColor"
+          stroke-dasharray="40"
+          stroke-dashoffset="40"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M8 6l10 6l-10 6Z"
+        >
+          <animate
+            fill="freeze"
+            attributeName="fill-opacity"
+            begin="0.5s"
+            dur="0.5s"
+            values="0;1"
+          />
+          <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.5s" values="40;0" />
+        </path>
+      </svg>
+      <svg
+        v-else
+        @click="pause()"
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+      >
+        <path
+          fill="currentColor"
+          d="M15 18q-.402 0-.701-.299T14 17V7q0-.402.299-.701T15 6h1.5q.402 0 .701.299T17.5 7v10q0 .402-.299.701T16.5 18zm-7.5 0q-.402 0-.701-.299T6.5 17V7q0-.402.299-.701T7.5 6H9q.402 0 .701.299T10 7v10q0 .402-.299.701T9 18z"
+        />
+      </svg>
+    </div>
     <div
       class="flex mt-4 gap-4 *:bg-blue-600 disabled:*:bg-gray-300 *:px-2 *:py-1 *:rounded *:text-white"
     >
